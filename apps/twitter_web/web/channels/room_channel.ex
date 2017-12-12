3c592e0955278b20846087e9168722869540c09b
Anirudh <anirudh.pathak1@gmail.com>
2017-12-12 03:01:07 -0500

    #buggy Tweet infra supported. Need to debug why 2nd tweet even on the same page fails

--- apps/twitter_web/web/channels/room_channel.ex
+++ apps/twitter_web/web/channels/room_channel.ex
@@ -1,9 +1,11 @@
 defmodule TwitterWeb.RoomChannel do
     use Phoenix.Channel
     #all actions will be here
+    #state: {%userid -> userid}
   
     def join("room:lobby", _message, socket) do
-      {:ok, socket}
+        engine_pid = :global.whereis_name(:engine)
+        {:ok, assign(socket, :engine_pid, engine_pid)}
     end
 
     # def join("room:" <> _private_room_id, _params, _socket) do
@@ -12,18 +14,28 @@ defmodule TwitterWeb.RoomChannel do
 
     #channel.push("new_msg", {body: chatInput.value})
     def handle_in("new_msg", %{"body" => body}, socket) do
-        engine_pid = :global.whereis_name(:engine)
-        cond do
+        #TODO: add this to socket state, in join maybe
+        engine_pid = socket.assigns[:engine_pid]
+
+        res = cond do
             body == "register" -> 
-                userid = GenServer.call(engine_pid, :register) |> Integer.to_string
-                res = "Registered. Your userid is #{userid}"
-                broadcast! socket, "new_msg", %{body: res}
-                {:noreply, assign(socket, :userid, userid)}
+                userid = GenServer.call(engine_pid, :register)
+                socket = assign(socket, :userid, userid)
+                "Registered. Your userid is #{userid |> Integer.to_string}"
+            String.starts_with? body, "tweet:" -> 
+                tweet_content = body |> String.slice(6..139) |> String.trim()
+                userid = socket.assigns[:userid]
+                if userid == nil do
+                    "You have not registered. First register by typing 'register' in textbox"
+                else
+                    :ok = GenServer.call(engine_pid, {:tweet, userid, tweet_content}, :infinity)
+                    "Tweeted: #{tweet_content}"
+                end
             true -> 
-                res = "unsupported command: " <> body
-                broadcast! socket, "new_msg", %{body: res}
-                {:noreply, socket}
+                "unsupported command: " <> body
         end
+        broadcast! socket, "new_msg", %{body: res}
+        {:noreply, socket}
     end
 
   end
