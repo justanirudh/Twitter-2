f450db06d86f8fbfef43ee472125ad3514d6d125
Anirudh <anirudh.pathak1@gmail.com>
2017-12-12 02:35:19 -0500

    Save userid in socket

--- apps/twitter_web/web/channels/room_channel.ex
+++ apps/twitter_web/web/channels/room_channel.ex
@@ -13,14 +13,17 @@ defmodule TwitterWeb.RoomChannel do
     #channel.push("new_msg", {body: chatInput.value})
     def handle_in("new_msg", %{"body" => body}, socket) do
         engine_pid = :global.whereis_name(:engine)
-        res = cond do
-            (body == "register") -> 
+        cond do
+            body == "register" -> 
                 userid = GenServer.call(engine_pid, :register) |> Integer.to_string
-                "Registered. Your userid is #{userid}. You can use this for your further queries"
-            true -> "unsupported command: " <> body     
+                res = "Registered. Your userid is #{userid}"
+                broadcast! socket, "new_msg", %{body: res}
+                {:noreply, assign(socket, :userid, userid)}
+            true -> 
+                res = "unsupported command: " <> body
+                broadcast! socket, "new_msg", %{body: res}
+                {:noreply, socket}
         end
-        broadcast! socket, "new_msg", %{body: res}
-        {:noreply, socket}
     end
 
   end
